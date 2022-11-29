c1fb7f0ef8dd9706504a840c1a283e1338b6f1cb
Konstantino123 <kt79@bk.ru>
2022-11-29 16:19:16 +0300

    change array

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Задача 54:
+Задайте двумерный массив.
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+int m, n; 
+ int value = 1; 
+ Console.Write("Введите кол-во строк "); 
+ m = Int32.Parse(Console.ReadLine()); 
+ Console.Write("Введите кол-во столбцов "); 
+ n = Int32.Parse(Console.ReadLine()); 
+ Random rnd = new Random(); 
+ int[,] numbs = new int[m, n]; 
+ int row = rnd.Next(numbs.GetLength(0)+1); 
+ int col = rnd.Next(numbs.GetLength(1)+1); 
+ for (int i = 0; i<m; i++) 
+ { 
+ for (int j = 0; j < n; j++) 
+ { 
+ numbs[i, j] = value; 
+ Console.Write(numbs[i, j] + " "); 
+ value++; 
+ } 
+ Console.WriteLine(); 
+ } 
+ void PrintArray(int [,] tempArray)
+{
+    for (int i = 0; i < tempArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempArray.GetLength(1); j++)
+        {
+            Console.Write($"{tempArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] newArray = ChangeArray(array: numbs, m: row, n: col);
+int[,] ChangeArray(int[,] array, int m, int n)
+{
+    int temp = new int();
+        
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < (array.GetLength(1) - 1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if((array[i, j] < array[i, k]))
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+             
+        }   
+    }
+return array;
+}
+PrintArray(newArray);
